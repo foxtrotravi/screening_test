a513a8c168383dc6bca1c0b24da92eff91d6432e
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2023-01-02 09:38:23 +0530

    Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:screening_test/ui/home_page.dart';
-import 'package:screening_test/utils/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
